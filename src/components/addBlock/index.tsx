8837a4572713b4262b03a1c2ecb1e144b6781330
alidadaashi <alidadaashi@gmail.com>
2023-08-10 14:01:19 +0300

    add context to addBlock component

--- src/components/addBlock/index.tsx
+++ src/components/addBlock/index.tsx
@@ -1,7 +1,17 @@
+import AppContext from '@/shared/context/appContext';
+import { useContext } from 'react';
+
 const AddBlock: React.FC = () => {
+  const { guestMode } = useContext(AppContext);
   return (
     <div>
-      <h1>AddBlock</h1>
+      {!guestMode && (
+        <div>
+          <button className="fixed right-8 bottom-8 rounded-full bg-green-700 hover:bg-green-800 w-12 h-12 text-white font-medium text-4xl flex justify-center items-center">
+            <span className="-mt-1">+</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
