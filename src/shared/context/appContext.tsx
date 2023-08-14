6431fd27c139a58c5e784043b2f8eacc4cbc17c8
alidadaashi <alidadaashi@gmail.com>
2023-08-14 18:18:40 +0300

    add correctOrderNumbers function to context

--- src/shared/context/appContext.tsx
+++ src/shared/context/appContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useEffect, useState } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { AppState, Block } from '../types/app';
 const AppContext = createContext<AppState>({
   guestMode: true,
@@ -29,10 +35,23 @@ export const AppProvider = ({
   const [mode, setMode] = useState<AppState['guestMode']>(true);
   const [blocks, setBlocks] =
     useState<AppState['blocks']>(initialBlocks);
-  const handleBlocks = (blocks: Block[]) => setBlocks(blocks);
+  const handleBlocks = (blocks: Block[]) => {
+    const orderedBlocks = correctOrderNumbers(blocks);
+    setBlocks(orderedBlocks);
+  };
   useEffect(() => {
     localStorage.setItem('blocks', JSON.stringify(blocks));
   }, [blocks]);
+
+  const correctOrderNumbers = useCallback((blocks: Block[]) => {
+    const orderedBlocks = blocks.map((block, index) => {
+      return {
+        ...block,
+        order: index + 1,
+      };
+    });
+    return orderedBlocks;
+  }, []);
 
   return (
     <AppContext.Provider
